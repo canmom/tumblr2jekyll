3afcff8fa8607aeea556fd533a7ee9c049601313
canmom <bryn.dickinson@gmail.com>
2017-07-24 20:00:45 +0100

    Add 'blog info' function to utils and refactor
    Abstract 'get' calls to the Tumblr API that only require a single key
    to a single function.
    
    Use this to define a function to get blog info with the /info API
    method. Can be extended to other such calls.

--- util.py
+++ util.py
@@ -6,15 +6,23 @@ with open('api-keys.txt') as keyfile:
     keys = [key.strip() for key in keyfile]
     keyfile.close()
 
-def posts_url(blog):
-    return "{0}{1}.tumblr.com/posts".format(base_url,blog,keys[0])
+def api_url(blog,method):
+    return "{0}{1}.tumblr.com/{2}".format(base_url,blog,method)
 
-def get_posts(blog,params):
-    url = posts_url(blog)
+def get(blog,method,params):
+    url = api_url(blog,method)
     params.update({'api_key': keys[0]})
 
     response = requests.get(url,params=params)
     response.raise_for_status()
 
-    posts = response.json()['response']['posts']
-    return posts
+    print('response received at',response.url)
+
+    return response.json()['response']
+
+def get_posts(blog,params):
+    return get(blog,'posts',params)['posts']
+
+def get_bloginfo(blog,params):
+    return get(blog,'info',params)['blog']
+    
