aee9e71844cd3154436d68f2bfe7659f77f60893
canmom <bryn.dickinson@gmail.com>
2017-08-15 23:02:58 +0100

    Deal with posts originating from multiple blogs
    The use case: I wanted to download all the posts reblogged to
    dldqdot.tumblr.com, but the forms originally posted to canmom.tumblr.com
    However, I renamed canmom from canonicalmomentum so some reblogs list
    canonicalmomentum. This allows a set of multiple URLs to be scanned for.

--- sources.py
+++ sources.py
@@ -3,14 +3,16 @@ from argparse import ArgumentParser
 import util
 
 #go up the reblog history until you find the most recent instance of target_blog
-def get_source_id(post,target_blog):
+def get_source_id(post,target_blogs):
     for past_reblog in reversed(post['trail']):
-        if past_reblog['blog']['name'] == target_blog:
+        if past_reblog['blog']['name'] in target_blogs:
             return past_reblog['post']['id']
-    print('No reblog from',target_blog,'in trail!')
+
+    #if it reaches the end of the loop
+    print('No reblog from {blog} in trail of post {url} - skipping...'.format(blog=target_blogs,url=post['post_url']))
     return None
 
-def get_all_source_ids(blog, original_blog):
+def get_all_source_ids(blog, original_blogs):
     #determine the number of posts
     bloginfo = util.get_bloginfo(blog,dict())
 
@@ -29,7 +31,7 @@ def get_all_source_ids(blog, original_blog):
         print(len(posts),'posts returned')
 
         for post in posts:
-            source_id = get_source_id(post, original_blog)
+            source_id = get_source_id(post, original_blogs)
             if source_id is not None:
                 source_ids.add(source_id)
 
--- tumblr2jekyll.py
+++ tumblr2jekyll.py
@@ -1,20 +1,20 @@
 from argparse import ArgumentParser
 
 from downloader import save_post
-from list_ids import get_all_source_ids
+from sources import get_all_source_ids
 
 # read id off command line
 parser = ArgumentParser(description='Download Tumblr text posts and format for Jekyll.')
 parser.add_argument('--id',help='ID of the post to download',default=None)
 parser.add_argument('--reblogger',help='Name of Tumblr blog containing reblogs of all posts to download',default='dldqdot')
 parser.add_argument('--source',help='Name of Tumblr blog containing source posts to download',default='canmom')
-parser.add_argument('--prev_source_url',help='Previous url of source that may appear in reblog trails',default='canonicalmomentum')
-args=parser.parse_args()
+parser.add_argument('--prev_source_url',help='Previous url(s) of source that may appear in reblog trails',default={'canonicalmomentum','canmom'},nargs='*')
+args = parser.parse_args()
 
 if args.id is not None:
     save_post(args.source, args.id)
 else:
-    post_ids = get_all_source_ids(args.reblogger, args.prev_source_url)
+    post_ids = get_all_source_ids(args.reblogger, set(args.prev_source_url))
 
     for post_id in post_ids:
         print('Identified post',post_id)
